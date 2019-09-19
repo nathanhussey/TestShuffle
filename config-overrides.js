4f4838c53ebddfe80fe17a859f65f93aa29fc7b7
Nathan Hussey <husseynathan@gmail.com>
2019-09-18 20:22:40 -0400

    changes to config color

--- config-overrides.js
+++ config-overrides.js
@@ -10,7 +10,6 @@
 		  libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
 		}),
 		addLessLoader({
-		  javascriptEnabled: true,
-		  modifyVars: { "@primary-color": "#1DA57A" }
+		  javascriptEnabled: true
 		})
 	  );
