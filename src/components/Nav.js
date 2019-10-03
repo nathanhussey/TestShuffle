ebf40d81db7501169d6502f905ecb17826ea5957
Nathan Hussey <husseynathan@gmail.com>
2019-10-02 23:25:14 -0400

    add mobile nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Button } from "antd";
+import { useState } from "react";
 import "./Nav.css";
 import logo from "../images/test-shuffle-logo.png";
 
 const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    console.log("clicked");
+    switch (showMenu) {
+      case false:
+        setShowMenu(true);
+        break;
+      case true:
+        setShowMenu(false);
+        break;
+      default:
+        console.log("error with toggle");
+    }
+  };
+  console.log(showMenu);
+  let toggleMenu;
+  if (showMenu === true) {
+    toggleMenu = (
+      <div className="mobile-menu">
+        <Link className="mobile-menu-link" to="/signup">
+          Sign Up
+        </Link>
+        <Link className="mobile-menu-link" to="/login">
+          Log In
+        </Link>
+      </div>
+    );
+  } else {
+  }
   return (
     <div>
+      <div className="nav-margins-mobile">
+        <Link to="/">
+          <img src={logo} className="logo"></img>
+        </Link>
+        <label onClick={handleToggleMenu}>&#9776;</label>
+      </div>
+      {toggleMenu}
       <div className=" nav-margins">
         <div>
           <Link to="/">
